8a7bd220eb0d9af98abca47e1ee2c953ba4c8951
Felipe Pinheiro <flpinheiro@gmail.com>
2021-04-16 10:37:44 -0300

    change consulOptions from set to init

--- WebClientConsul/Configuration/Consul/ConsulOptions.cs
+++ WebClientConsul/Configuration/Consul/ConsulOptions.cs
@@ -5,14 +5,14 @@ namespace WebClientConsul.Configuration.Consul
 {
     public class ConsulOptions
     {
-        public string Id { get; set; } = Guid.NewGuid().ToString();
-        public bool Enabled { get; set; }
-        public string Host { get; set; } = "http://127.0.0.1:8500/";
-        public string Service { get; set; }
-        public string Address { get; set; }
-        public int Port { get; set; }
-        public bool PingEnabled { get; set; }
-        public string[] Tags { get; set; }
-        public IDictionary<string, string> MetaData { get; set; }
+        public string Id { get; init; } = Guid.NewGuid().ToString();
+        public bool Enabled { get; init; }
+        public string Host { get; init; } = "http://localhost:8500/";
+        public string Service { get; init; }
+        public string Address { get; init; }
+        public int Port { get; init; }
+        public bool PingEnabled { get; init; }
+        public string[] Tags { get; init; }
+        public IDictionary<string, string> MetaData { get; init; }
     }
 }
